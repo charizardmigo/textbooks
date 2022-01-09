89888fc3463c348e839a58d8ba09b9543608e6b9
charizardmigo <fomechepriestly7@gmail.com>
2022-01-09 23:37:00 +0100

    Solution to Exercise 2

---
+++ c-modern-approach/chapter2/exercises/exercise2.c
@@ -0,0 +1,36 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 2, Exercise 2, Page 33
+
+PRIESTLEY FOMECHE
+5TH JANUARY 2022
+
+Q: Consider the following program:
+   (a) Identify the directives and statements hi this program.
+   (b) What output does the program produce?
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Parkinson's Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n") ;
+
+    return 0;
+}
+
+/*
+
+A:
+
+(a) The program contains one directive (#include) and four statements (the three calls of printf and one return).
+
+(b) Parkinson's Law:
+    Work expands so as to fill the time
+    available for its completion.
+
+*/
