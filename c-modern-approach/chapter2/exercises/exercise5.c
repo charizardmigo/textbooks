ffb2929cfef4599309d43f07ecb33cdea7e0929f
charizardmigo <fomechepriestly7@gmail.com>
2022-01-10 06:48:46 +0100

    Solution to Exercise 5

---
+++ c-modern-approach/chapter2/exercises/exercise5.c
@@ -0,0 +1,17 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 2, Exercise 5, Page 33
+
+PRIESTLEY FOMECHE
+5TH JANUARY 2022
+
+Q: Which of the following are not legal C identifiers?
+   (a) 100_bottles
+   (b) _100_bottles
+   (c) one__hundred__bottles
+   (d) bottles_by_the_hundred_
+
+A: 100_bottles (a) is illegal because it begins with a digit
+
+*/
