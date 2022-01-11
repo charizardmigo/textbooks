e1249154f69bc3635446dac04b6b892782fc54da
charizardmigo <fomechepriestly7@gmail.com>
2022-01-12 00:26:25 +0100

    Solution to Exercise 6

---
+++ c-modern-approach/chapter2/exercises/exercise6.c
@@ -0,0 +1,13 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 2, Exercise 6, Page 33
+
+PRIESTLEY FOMECHE
+5TH JANUARY 2022
+
+Q: Why is it not a good idea for an identifier to contain more than one adjacent underscore (as in current__balance, for example)?
+
+A: Identifiers with more than one adjacent underscores can easily be typed incorrectly and make compilation errors difficult to find
+
+*/
