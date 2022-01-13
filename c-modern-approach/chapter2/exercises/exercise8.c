60e1dd69757e2e30081f707c6869e63c34b774f0
charizardmigo <fomechepriestly7@gmail.com>
2022-01-13 22:49:02 +0100

    Solution to Exercise 8

---
+++ c-modern-approach/chapter2/exercises/exercise8.c
@@ -0,0 +1,29 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 2, Exercise 8, Page 34
+
+PRIESTLEY FOMECHE
+5TH JANUARY 2022
+
+Q: How many tokens are there in the following statement?
+   answer=(3*q-p*p)/3;
+
+A: There are 14 tokens:
+
+   1. answer
+   2. =
+   3. (
+   4. 3
+   5. *
+   6. q
+   7. -
+   8. p
+   9. *
+   10. p
+   11. )
+   12. /
+   13. 3
+   14. ;
+
+*/
