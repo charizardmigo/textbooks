c01b779d3969f99c2dac8469d76bde240b42664d
charizardmigo <fomechepriestly7@gmail.com>
2022-01-14 04:26:06 +0100

    Solution to Exercise 9

---
+++ c-modern-approach/chapter2/exercises/exercise9.c
@@ -0,0 +1,15 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 2, Exercise 9, Page 34
+
+PRIESTLEY FOMECHE
+5TH JANUARY 2022
+
+Q: Insert spaces between the tokens in Exercise 8 to make the statement easier to read.
+
+A: Initial statement: answer=(3*q-p*p)/3;
+
+   Statement with spaces: answer = (3 * q - p * p) / 3;
+
+*/
