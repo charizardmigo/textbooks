22c1d29020a1998f0550b13d3d48361fa091f090
charizardmigo <fomechepriestly7@gmail.com>
2022-01-16 08:34:04 +0100

    Displays affirmation symbol

---
+++ c-modern-approach/chapter2/projects/project1.c
@@ -0,0 +1,18 @@
+//PRIESTLEY FOMECHE
+//DISPLAYS AFFIRMATION SYMBOL
+//5TH JANUARY 2022
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *");
+
+    return 0;
+}
