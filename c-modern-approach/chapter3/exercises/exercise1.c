4f20a30e4e1e14e560f9e45746744a0d9279faca
charizardmigo <fomechepriestly7@gmail.com>
2022-01-20 08:32:29 +0100

    Solution to Exercise 1

---
+++ c-modern-approach/chapter3/exercises/exercise1.c
@@ -0,0 +1,21 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 3, Exercise 1, Page 49
+
+PRIESTLEY FOMECHE
+6TH JANUARY 2022
+
+Q: What output do the following calls of print produce
+   (a) printf ("%6d, %4d", 86, 1040);
+   (b) printf("%12.5e", 30.253);
+   (c) printf("%.4f", 83.162);
+   (d) printf("%-6.2g", .0000009979);
+
+A:
+   (a) ****86 (where * stands for 1 space)
+   (b) *3.02530e+01
+   (c) 83.1620
+   (d) 1e-06
+
+*/
