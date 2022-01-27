972533633326dd9d49c7af52df6279b06f7ce9a6
charizardmigo <fomechepriestly7@gmail.com>
2022-01-27 02:35:28 +0100

    Solution to Exercise 12

---
+++ c-modern-approach/chapter4/exercises/exercise_12.c
@@ -0,0 +1,35 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 4, Exercise 12, Page 70
+
+PRIESTLEY FOMECHE
+10TH JANUARY 2022
+
+Q: Show the output produced by each of the following program fragments.
+   Assume that i and j are int variables.
+
+   (a) i = 5;
+       j = ++i * 3 - 2;
+       printf("%d %d", i, j);
+
+   (b) i = 5;
+       j = 3 - 2 * i++;
+       printf("%d %d", i, j);
+
+   (c) i = 7;
+       j = 3 * i-- + 2;
+       printf("%d %d", i, j);
+
+   (d) i = 7;
+       j = 3 + --i * 2;
+       printf("%d %d", i, j);
+
+
+A:
+   (a) 6  16
+   (b) 6  -7
+   (c) 6  23
+   (d) 6  15
+
+*/
