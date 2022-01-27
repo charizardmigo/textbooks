cceca936fc8ac15e9f9055d34aa652ab3f03bed0
charizardmigo <fomechepriestly7@gmail.com>
2022-01-28 00:20:02 +0100

    Solution to Exercise 13

---
+++ c-modern-approach/chapter4/exercises/exercise_13.c
@@ -0,0 +1,14 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 4, Exercise 13, Page 70
+
+PRIESTLEY FOMECHE
+10TH JANUARY 2022
+
+Q: Only one of the expressions ++i and i++ is exactly the same as (i += 1); which is it?
+   Justify your answer.
+
+A: The expression ++i is equivalent to (i += 1). Both expressions increment i immediately.
+
+*/
