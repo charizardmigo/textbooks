de6cb8263b5a9cdee0ac95b0f3c860e2a01c899d
charizardmigo <fomechepriestly7@gmail.com>
2022-01-28 00:21:19 +0100

    Solution to Exercise 15

---
+++ c-modern-approach/chapter4/exercises/exercise_15.c
@@ -0,0 +1,23 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 4, Exercise 15, Page 71
+
+PRIESTLEY FOMECHE
+10TH JANUARY 2022
+
+Q: Give the values of i and j after each of the following expression statements has been executed.
+   (Assume that i has the value 1 initially and j has the value 2).
+
+   (a) i += j;
+   (b) i--;
+   (c) i * j / i;
+   (d) i % ++j;
+
+A:
+   (a) 3
+   (b) 1
+   (c) 2
+   (d) 1
+
+*/
