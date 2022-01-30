a3687d74ebe9033e3586a3db1c7deab33a44863b
charizardmigo <fomechepriestly7@gmail.com>
2022-01-30 12:19:57 +0100

    Prints a 3-digit number in reverse

---
+++ c-modern-approach/chapter4/projects/project3.c
@@ -0,0 +1,18 @@
+//PRIESTLEY FOMECHE
+//PRINTS A 3-DIGIT NUMBER FROM USER IN REVERSE
+//10TH JANUARY 2022
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num1, num2, num3;
+
+    printf("Enter a 3-digit number: ");
+    scanf("%1d%1d%1d", &num1, &num2, &num3);
+
+    printf("\nThe reverse is %d%d%d\n", num3, num2, num1);
+
+    return 0;
+}
