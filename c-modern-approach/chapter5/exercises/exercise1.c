308adae93a7e76c3d4fa2edf79103269933dbfe0
charizardmigo <fomechepriestly7@gmail.com>
2022-01-31 11:50:17 +0100

    Solution to Exercise 1

---
+++ c-modern-approach/chapter5/exercises/exercise1.c
@@ -0,0 +1,31 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 5, Exercise 1, Page 93
+
+PRIESTLEY FOMECHE
+14TH JANUARY 2022
+
+Q: The following program fragments illustrate the relational and equality operators.
+   Show the output produced by each, assuming that i, j and k are int variables.
+
+   (a) i = 2; j = 3;
+       k = i * j == 6;
+       printf("%d", k);
+
+   (b) i = 5; j = 10, k = 1;
+       printf("%d", k > i < j);
+
+   (c) i = 3; j = 2; k = 1;
+       printf("%d", i < j == j < k);
+
+   (d) i = 3; j = 4; k = 5;
+       printf("%d", i % j + i < k);
+
+
+A: (a) 6
+   (b) 1
+   (c) 0
+   (d) 0
+
+*/
