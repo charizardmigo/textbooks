c11e5fa313b9455665f54153da11e32deac8c620
charizardmigo <fomechepriestly7@gmail.com>
2022-02-01 10:18:30 +0100

    Solution to Exercise 4

---
+++ c-modern-approach/chapter5/exercises/exercise4.c
@@ -0,0 +1,15 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 5, Exercise 4, Page 94
+
+PRIESTLEY FOMECHE
+14TH JANUARY 2022
+
+Q: Write a single expression whose value is either -1, 0 or +1 depending on whether i is
+   less than, equal to or greater than j respectively.
+
+
+A: (i > j) - (i < j)
+
+*/
