91c812decb5e78426494ed0baa4d2ffc4c7884ed
charizardmigo <fomechepriestly7@gmail.com>
2022-02-01 10:15:14 +0100

    Solution to Exercise 2

---
+++ c-modern-approach/chapter5/exercises/exercise2.c
@@ -0,0 +1,30 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 5, Exercise 2, Page 94
+
+PRIESTLEY FOMECHE
+14TH JANUARY 2022
+
+Q: The following program fragments illustrate the logical operators.
+   Show the output produced by each, assuming that i, j and k are int variables.
+
+   (a) i = 10; j = 5;
+       printf("%d", !i < j);
+
+   (b) i = 2; j = 1;
+       printf("%d", !!i + !j);
+
+   (c) i = 5; j = 0; k = -5;
+       printf("%d", i && j || k);
+
+   (d) i = 1; j = 2; k = 3;
+       printf("%d", i < j || k);
+
+
+A: (a) 1
+   (b) 1
+   (c) 1
+   (d) 1
+
+*/
