5e9d26c1401a9fb41a5127fe8b7335ed4fd397eb
charizardmigo <fomechepriestly7@gmail.com>
2022-02-02 00:26:16 +0100

    Solution to Exercise 7

---
+++ c-modern-approach/chapter5/exercises/exercise7.c
@@ -0,0 +1,17 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 5, Exercise 7, Page 94
+
+PRIESTLEY FOMECHE
+14TH JANUARY 2022
+
+Q: What does the following statement print, if i has the value 17?
+   What does it print if i has the value -17?
+
+   printf("%d\n", i >= 0 ? i : -i;
+
+
+A: It prints 17 in both cases of i.
+
+*/
