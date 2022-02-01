d186e450d9cfa4d1f1a60be7427accde489aeb92
charizardmigo <fomechepriestly7@gmail.com>
2022-02-02 00:25:33 +0100

    Solution to Exercise 6

---
+++ c-modern-approach/chapter5/exercises/exercise6.c
@@ -0,0 +1,18 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 5, Exercise 6, Page 94
+
+PRIESTLEY FOMECHE
+14TH JANUARY 2022
+
+Q: Is the following if statement legal?
+   if (n == 1 - 10)
+    printf("n is between 1 and 10\n");
+
+   If so, what does it do when n is equal to 5?
+
+
+A: Yes, the statement is legal. When n is equal to 5, it does nothing, since 5 is not equal to –9.
+
+*/
