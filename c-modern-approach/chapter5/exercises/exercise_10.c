9e0b230e219f875e3ef68930193b55f8a992e870
charizardmigo <fomechepriestly7@gmail.com>
2022-02-03 08:52:33 +0100

    Solution to Exercise 10

---
+++ c-modern-approach/chapter5/exercises/exercise_10.c
@@ -0,0 +1,22 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 5, Exercise 10, Page 95
+
+PRIESTLEY FOMECHE
+14TH JANUARY 2022
+
+Q: What output does the following program fragment produce?
+   (Assume that i is an integer variable).
+
+   i = 1;
+   switch (i % 3) {
+       case 0: printf("zero");
+       case 1: printf("one");
+       case 2: printf("two");
+   }
+
+
+A: The answer is onetwo since there are no break statements after the cases.
+
+*/
