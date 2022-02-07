f5e88ecb3bd82fad9c7eb1a7e9211126f7149bdf
charizardmigo <fomechepriestly7@gmail.com>
2022-02-07 01:36:47 +0100

    Solution to Exercise 1

---
+++ c-modern-approach/chapter6/exercises/exercise1.c
@@ -0,0 +1,20 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 1, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: What output does the following program fragment produce?
+
+   i = 1;
+   while (i <= 128) {
+    printf("%d ", i);
+    i *= 2;
+   }
+
+
+A: 1 2 4 8 16 32 64 128
+
+*/
