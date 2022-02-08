a777ae657bc26e64ff0ae5aae1b2b21fcf3a3112
charizardmigo <fomechepriestly7@gmail.com>
2022-02-08 03:42:23 +0100

    Solution to Exercise 2

---
+++ c-modern-approach/chapter6/exercises/exercise2.c
@@ -0,0 +1,20 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 2, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: What output does the following program fragment produce?
+
+   i = 9384;
+    do {
+         printf("%d ", i);
+         i /= 10;
+   } while (i > 0);
+
+
+A: 9384 938 93
+
+*/
