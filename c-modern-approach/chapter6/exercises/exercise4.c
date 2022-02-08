6d7fcfc8adc336700d896762c1f56e5788f662ed
charizardmigo <fomechepriestly7@gmail.com>
2022-02-08 03:45:26 +0100

    Solution to Exercise 4

---
+++ c-modern-approach/chapter6/exercises/exercise4.c
@@ -0,0 +1,20 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 4, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: Which of the following statements is not equivalent to the other two
+   (assuming that the loop bodies are the same)?
+
+   (a) for (i = 0; i < 10; i++) ...
+   (b) for (i = 0; i < 10; ++i) ...
+   (c) for (i = 0; i++ < 10; ) ...
+
+
+A: (c) is not equivalent to (a) and (b), because i is incremented before the loop body is executed.
+
+
+*/
