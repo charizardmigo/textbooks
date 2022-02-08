f235b6b12fbf7239c0bf93f1e34e89b0f0ec2c22
charizardmigo <fomechepriestly7@gmail.com>
2022-02-08 03:46:18 +0100

    Solution to Exercise 5

---
+++ c-modern-approach/chapter6/exercises/exercise5.c
@@ -0,0 +1,20 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 5, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: Which of the following statements is not equivalent to the other two
+   (assuming that the loop bodies are the same)?
+
+   (a) while (i < 10) {...}
+   (b) for (; i < 10;) {...}
+   (c) do {...} while (i < 10);
+
+
+A: (c) is not equivalent to (a) and (b), because the loop body is executed first before the condition is tested.
+
+
+*/
