ea84b68aef1ba3c7969c9e05cf22d03a1c742c31
charizardmigo <fomechepriestly7@gmail.com>
2022-02-08 03:44:07 +0100

    Solution to Exercise 3

---
+++ c-modern-approach/chapter6/exercises/exercise3.c
@@ -0,0 +1,17 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 3, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: What output does the following for statement produce?
+
+   for (i = 5, j = i - 1; i > 0, j > 0; --i, j = i - 1){
+    printf("%d ", i);
+   }
+
+A: 4
+
+*/
