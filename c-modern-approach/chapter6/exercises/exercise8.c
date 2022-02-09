03743f2e595d26afd3fe041937a85a3f17e10352
charizardmigo <fomechepriestly7@gmail.com>
2022-02-09 05:24:50 +0100

    Solution to Exercise 8

---
+++ c-modern-approach/chapter6/exercises/exercise8.c
@@ -0,0 +1,18 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 8, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: What output does the following for statement produce?
+
+   for (i = 10; i >= 1; i /= 2)
+      printf("%d ", i++);
+
+
+A: 10 6 4 3 2 1
+
+
+*/
