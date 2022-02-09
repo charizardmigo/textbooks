48c721905608ac841f4b6a9800c95b0ec108af6c
charizardmigo <fomechepriestly7@gmail.com>
2022-02-09 05:25:21 +0100

    Solution to Exercise 9

---
+++ c-modern-approach/chapter6/exercises/exercise9.c
@@ -0,0 +1,21 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 9, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: Translate the for statement of Exercise 8 into an equivalent while statement.
+   You will need one statement in addition to the while loop itself.
+
+
+A:
+    int i = 10;
+    while (i >= 1){
+        printf("%d ", i++);
+        i /= 2;
+    }
+
+
+*/
