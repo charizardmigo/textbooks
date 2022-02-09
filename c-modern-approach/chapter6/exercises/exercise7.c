92da1fc83b77e6054d0a8e4c3ae37d66b79a328e
charizardmigo <fomechepriestly7@gmail.com>
2022-02-09 05:24:21 +0100

    Solution to Exercise 7

---
+++ c-modern-approach/chapter6/exercises/exercise7.c
@@ -0,0 +1,15 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 7, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: Translate the program fragment of Exercise 2 into a single for statement.
+
+
+A:  for (i = 9384; i > 0; i /= 10)
+       printf("%d ", i);
+
+*/
