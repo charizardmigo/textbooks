9f93492c3d793bcdb643197d58ce3367ead85b0c
charizardmigo <fomechepriestly7@gmail.com>
2022-02-09 05:23:30 +0100

    Solution to Exercise 6

---
+++ c-modern-approach/chapter6/exercises/exercise6.c
@@ -0,0 +1,15 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 6, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: Translate the program fragment of Exercise 1 into a single for statement.
+
+
+A:  for (i = 1; i <= 128; i *= 2)
+       printf("%d ", i);
+
+*/
