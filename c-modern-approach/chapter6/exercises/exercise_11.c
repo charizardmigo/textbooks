fc8ac762b50dd61d1ecd0f14d362c9b037791408
charizardmigo <fomechepriestly7@gmail.com>
2022-02-10 15:39:42 +0100

    Solution to Exercise 11

---
+++ c-modern-approach/chapter6/exercises/exercise_11.c
@@ -0,0 +1,23 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 11, Page 121
+
+PRIESTLEY FOMECHE
+25TH JANUARY 2022
+
+Q: What does the following program fragment produce?
+
+    sum = 0;
+    for (i = 0; i < 10; i++) {
+        if (i % 2)
+            continue;
+        sum += i;
+    }
+    printf("%d\n", sum);
+
+
+A: 20
+
+
+*/
