f0abcf0afbb2eed3e79947fb21eb32bb2c9329fc
charizardmigo <fomechepriestly7@gmail.com>
2022-02-11 02:49:59 +0100

    Solution to Exercise 14

---
+++ c-modern-approach/chapter6/exercises/exercise_14.c
@@ -0,0 +1,19 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 6, Exercise 14, Page 122
+
+PRIESTLEY FOMECHE
+26TH JANUARY 2022
+
+Q: Find the error in the following program fragment and fix it:
+
+    if (n % 2 == 0);
+        printf("n is even\n");
+
+A: The problem is the semicolon at the end of the first line. If we remove it, the statement is now correct:
+
+     if (n % 2 == 0)
+        printf("n is even\n");
+
+*/
