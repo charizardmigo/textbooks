2bd5d201bf7eb6d5109c2bfb0b8869cfe980fa95
charizardmigo <fomechepriestly7@gmail.com>
2022-08-17 07:41:24 +0100

    Solution to Exercise 3

---
+++ c-modern-approach/chapter4/exercises/exercise3.c
@@ -0,0 +1,22 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 4, Exercise 3, Page 69
+
+PRIESTLEY FOMECHE
+10TH JANUARY 2022
+
+Q: What is the value of each of the following expressions in C89? (Give all possible values if an expression may have more than one value).
+
+(a) 8 / 5
+(b) -8 / 5
+(c) 8 / -5
+(d) -8 / -5
+
+
+A: (a) 1
+   (b) -1 or -2
+   (c) -1 or -2
+   (d) 1 or 2
+
+*/
