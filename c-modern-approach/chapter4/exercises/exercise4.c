81651bb60f3d57f7e1aca3367948dbe8ab6890ef
charizardmigo <fomechepriestly7@gmail.com>
2022-08-17 07:48:06 +0100

    Solution to Exercise 4

---
+++ c-modern-approach/chapter4/exercises/exercise4.c
@@ -0,0 +1,17 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 4, Exercise 4, Page 69
+
+PRIESTLEY FOMECHE
+10TH JANUARY 2022
+
+Q: Repeat Exercise 3 for C99.
+
+
+A: (a) 1
+   (b) -1
+   (c) -1
+   (d) 1
+
+*/
