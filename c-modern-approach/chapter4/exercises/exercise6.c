68691ce2982a147bc8162f305f10921b1db3ed96
charizardmigo <fomechepriestly7@gmail.com>
2022-08-17 07:53:07 +0100

    Solution to Exercise 6

---
+++ c-modern-approach/chapter4/exercises/exercise6.c
@@ -0,0 +1,17 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 4, Exercise 6, Page 69
+
+PRIESTLEY FOMECHE
+10TH JANUARY 2022
+
+Q: Repeat Exercise 5 for C99.
+
+
+A: (a) 3
+   (b) -3
+   (c) 3
+   (d) -3
+
+*/
