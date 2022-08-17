7d01a89b266584d5a70f91551aea3320f85506be
charizardmigo <fomechepriestly7@gmail.com>
2022-08-17 07:51:21 +0100

    Solution to EXercise 5

---
+++ c-modern-approach/chapter4/exercises/exercise5.c
@@ -0,0 +1,22 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 4, Exercise 5, Page 69
+
+PRIESTLEY FOMECHE
+10TH JANUARY 2022
+
+Q: What is the value of each of the following expressions in C89? (Give all possible values if an expression may have more than one value).
+
+(a) 8 % 5
+(b) -8 % 5
+(c) 8 % -5
+(d) -8 % -5
+
+
+A: (a) 3
+   (b) -3 or 5
+   (c) 3 or -5
+   (d) 3
+
+*/
