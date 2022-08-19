32518831f1042ca11aec3c60ed49d030146c7c0e
charizardmigo <fomechepriestly7@gmail.com>
2022-08-19 05:26:23 +0100

    Prints a number in reverse

--- c-modern-approach/chapter6/projects/project5.c
+++ c-modern-approach/chapter6/projects/project5.c
@@ -1,19 +1,20 @@
-//PRIESTLEY FOMECHE
-//PRINTS A NUMBER IN REVERSE
-//26TH JANUARY 2022
+// PRIESTLEY FOMECHE
+// PRINTS A NUMBER IN REVERSE
+// 26TH JANUARY 2022
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) 
+int main(void)
 {
-    int num, digits = 0;
+    int num;
 
     printf("Enter a number: ");
     scanf("%d", &num);
     printf("\nThe reverse is ");
 
-    do {
+    do
+    {
         printf("%d", num % 10);
         num /= 10;
 
