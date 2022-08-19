bac6d0e2b5eae1a6a913a08ec19a50163d105afd
charizardmigo <fomechepriestly7@gmail.com>
2022-08-19 05:22:35 +0100

    Solution to Exercise 13

--- c-modern-approach/chapter6/exercises/exercise_13.c
+++ c-modern-approach/chapter6/exercises/exercise_13.c
@@ -11,9 +11,9 @@ Q: Rewrite the following loop so that the loop body is empty:
     for (n = 0; m > 0; n++)
         m /= 2;
 
-A:
-    for (n = 0; m > 0 && m /= 2; n++)
-    ;
 
+A:
+    for (n = 0; m > 0; n++, m /= 2)
+    ;
 
 */
