256131856ce20165522bee9fff748c5604582caf
charizardmigo <fomechepriestly7@gmail.com>
2022-08-20 22:45:01 +0100

    Solution to Exercise 1

---
+++ c-modern-approach/chapter7/exercises/exercise1.c
@@ -0,0 +1,21 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 1, Page 155
+
+PRIESTLEY FOMECHE
+19TH AUGUST 2022
+
+Q: Give the decimal value of each of the following integer constants
+
+   a) 077
+   b) 0x77
+   c) 0XABC
+
+
+A:
+   a) 63
+   b) 119
+   c) 2748
+
+*/
