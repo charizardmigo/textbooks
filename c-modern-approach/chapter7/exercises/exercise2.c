07504d92843e23d523c422b77654ddbf2c1c84cf
charizardmigo <fomechepriestly7@gmail.com>
2022-08-20 22:50:49 +0100

    Solution to Exercise 2

---
+++ c-modern-approach/chapter7/exercises/exercise2.c
@@ -0,0 +1,25 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 2, Page 155
+
+PRIESTLEY FOMECHE
+19TH AUGUST 2022
+
+Q: Which of the following are not legal constants in C? Classify each legal constant as either
+   integer or floating-point.
+
+   a) 010E2
+   b) 32.1E+5
+   c) 0790
+   d) 100_00
+   e) 3.978e-2
+
+
+A:
+   a), b) and e) are legal floating-point constants.
+
+   c) and d) are illegal constants, because of undefined numeral 9
+   in the octal number 0790, and the underscore in 100_000.
+
+*/
