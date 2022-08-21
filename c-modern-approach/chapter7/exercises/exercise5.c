c50160bfbd8e5eedc86748635faa759b628d5467
charizardmigo <fomechepriestly7@gmail.com>
2022-08-21 23:47:44 +0100

    Solution to Exercise 5

---
+++ c-modern-approach/chapter7/exercises/exercise5.c
@@ -0,0 +1,20 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 5, Page 156
+
+PRIESTLEY FOMECHE
+19TH AUGUST 2022
+
+Q: Which of the following is not a legal way to write the number 65? (Assume the
+   character set is ASCII).
+
+   a) 'A'
+   b) 0b1000001
+   c) 0101
+   d) 0x41
+
+
+A: b) is illegal, as C does not allow binary represented numbers.
+
+*/
