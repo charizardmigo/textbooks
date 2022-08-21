5ac7c8982164faf96a009ff0034544b03aa88ced
charizardmigo <fomechepriestly7@gmail.com>
2022-08-21 23:46:56 +0100

    Solution to Exercise 4

---
+++ c-modern-approach/chapter7/exercises/exercise4.c
@@ -0,0 +1,19 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 4, Page 156
+
+PRIESTLEY FOMECHE
+19TH AUGUST 2022
+
+Q: If c is a variable of type char, which one of the following statements is illegal?
+
+   a) i += c;  //i has type int
+   b) c = 2 * c - 1;
+   c) putchar(c);
+   d) printf(c);
+
+
+A: d) is illegal because printf prints strings, not individual (numerical representations of) characters.
+
+*/
