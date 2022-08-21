6f97777ecb64ef985ef44322199593ce8596844d
charizardmigo <fomechepriestly7@gmail.com>
2022-08-21 23:50:31 +0100

    Solution to Exercise 8

---
+++ c-modern-approach/chapter7/exercises/exercise8.c
@@ -0,0 +1,18 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 8, Page 156
+
+PRIESTLEY FOMECHE
+21ST AUGUST 2022
+
+Q: Repeat Exercise 7, but give the equivalent hexadecimal escape.
+
+
+A:
+   a) \x08
+   b) \x0a
+   c) \x0d
+   d) \x09
+
+*/
