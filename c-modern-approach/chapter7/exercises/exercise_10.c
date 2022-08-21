c26d57d16ab136ca9a726ab265cfc9f613b08725
charizardmigo <fomechepriestly7@gmail.com>
2022-08-21 23:52:17 +0100

    Solution to Exercise 10

---
+++ c-modern-approach/chapter7/exercises/exercise_10.c
@@ -0,0 +1,15 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 10, Page 156
+
+PRIESTLEY FOMECHE
+21ST AUGUST 2022
+
+Q: Suppose that i is a variable of type int, j is a variable of type long, and k is a variable of
+   type unsigned int. What is the type of the expression i + (int) j * k?
+
+
+A: unsigned int, because the (int) cast applies only to j, not j * k.
+
+*/
