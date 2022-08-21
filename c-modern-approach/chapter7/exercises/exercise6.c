ee9e2510362c40697da97c83e7816b5959631971
charizardmigo <fomechepriestly7@gmail.com>
2022-08-21 23:48:28 +0100

    Solution to Exercise 6

---
+++ c-modern-approach/chapter7/exercises/exercise6.c
@@ -0,0 +1,24 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 6, Page 156
+
+PRIESTLEY FOMECHE
+21ST AUGUST 2022
+
+Q: For each of the following items of data, specify which one of the types char, short, int
+   or long is the smallest one guaranteed to be large enough to store the item.
+
+   a) Days in a month
+   b) Days in a year
+   c) Minutes in a day
+   d) Seconds in a day
+
+
+A:
+   a) char
+   b) short
+   c) short
+   d) long
+
+*/
