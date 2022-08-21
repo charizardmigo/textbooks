19c5a1499cebf19d934278d4e0ead54cc9d765cd
charizardmigo <fomechepriestly7@gmail.com>
2022-08-21 23:49:25 +0100

    Solution to Exercise 7

---
+++ c-modern-approach/chapter7/exercises/exercise7.c
@@ -0,0 +1,25 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 7, Page 156
+
+PRIESTLEY FOMECHE
+21ST AUGUST 2022
+
+Q: For each of the following character escapes, give the equivalent octal escape. (Assume that
+   the character set is ASCII). You may wish to consult Appendix E, which lists the numerical
+   codes for ASCII characters.
+
+   a) \b
+   b) \n
+   c) \r
+   d) \t
+
+
+A:
+   a) \10
+   b) \12
+   c) \15
+   d) \11
+
+*/
