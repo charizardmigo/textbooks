f84329347c5bc07585802fd26f288a14ebbaa257
charizardmigo <fomechepriestly7@gmail.com>
2022-08-21 23:51:22 +0100

    Solution to Exercise 9

---
+++ c-modern-approach/chapter7/exercises/exercise9.c
@@ -0,0 +1,14 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 9, Page 156
+
+PRIESTLEY FOMECHE
+21ST AUGUST 2022
+
+Q: Suppose that i and j are variables of type int. What is the type of the expression i / j + 'a'?
+
+
+A: int, because the expression automatically promotes the char value 'a' to an int.
+
+*/
