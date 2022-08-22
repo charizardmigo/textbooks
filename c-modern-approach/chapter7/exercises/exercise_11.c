2c14e583a7c24a5243094c6065c3eff5beb26016
charizardmigo <fomechepriestly7@gmail.com>
2022-08-22 23:19:53 +0100

    Solution to Exercise 11

---
+++ c-modern-approach/chapter7/exercises/exercise_11.c
@@ -0,0 +1,15 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 11, Page 156
+
+PRIESTLEY FOMECHE
+21ST AUGUST 2022
+
+Q: Suppose that i is a variable of type int, f is a variable of type float, and d is a variable of
+   type double. What is the type of the expression i * f / d?
+
+
+A: double
+
+*/
