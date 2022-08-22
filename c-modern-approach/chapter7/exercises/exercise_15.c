58d4a494c30e5f0d5e432bfc58eef9cac915f09b
charizardmigo <fomechepriestly7@gmail.com>
2022-08-22 23:25:03 +0100

    Solution to Exercise 15

---
+++ c-modern-approach/chapter7/exercises/exercise_15.c
@@ -0,0 +1,18 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 15, Page 157
+
+PRIESTLEY FOMECHE
+21ST AUGUST 2022
+
+Q: Use typedefs to create types named Int8, Int16 and Int32. Define the types so that
+   they represent 8-bit, 16-bit and 32-bit integers on your machine.
+
+
+A:
+    typedef char Int8;   // char uses 1 byte (8 bits) of memory
+    typedef short Int16;   // short uses 2 bytes (16 bits) of memory
+    typedef int Int32;   // int uses 4 bytes (32 bits) of memory
+
+*/
