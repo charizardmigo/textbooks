eabae421af20f370d454e612bf0e32621a1bbad3
charizardmigo <fomechepriestly7@gmail.com>
2022-08-22 23:21:43 +0100

    Solution to Exercise 12

---
+++ c-modern-approach/chapter7/exercises/exercise_12.c
@@ -0,0 +1,17 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 12, Page 156
+
+PRIESTLEY FOMECHE
+21ST AUGUST 2022
+
+Q: Suppose that i is a variable of type int, f is a variable of type float, and d is a variable of
+   type double. Explain what conversions take place during the execution of the following statement:
+
+   d = i + f;
+
+
+A: The value of i is converted to a float and added to f, then the result is stored in d as a double.
+
+*/
