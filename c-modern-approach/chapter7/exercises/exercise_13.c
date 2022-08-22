bddabb4703276a0b9f39b4aa0d27355a36a478ce
charizardmigo <fomechepriestly7@gmail.com>
2022-08-22 23:23:06 +0100

    Solution to Exercise 13

---
+++ c-modern-approach/chapter7/exercises/exercise_13.c
@@ -0,0 +1,32 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 7, Exercise 13, Page 157
+
+PRIESTLEY FOMECHE
+21ST AUGUST 2022
+
+Q: Assume that a program contains the following declarations:
+
+   char c = '\1';
+   short s = 2;
+   int i = -3;
+   long m = 5;
+   float f = 6.5f;
+   double d = 7.5;
+
+   Give the value and the type of each expression listed below.
+
+   (a) c * i    (c) f / c   (e) f - d
+   (b) s + m    (d) d / s   (f) (int) f
+
+
+A:
+   (a) Value = -3, Type = int
+   (b) Value = 7, Type = long
+   (c) Value = 6.5f, Type = float
+   (d) Value = 3.75, Type = double
+   (e) Value = -1.0, Type = double
+   (f) Value = 6, Type = int
+
+*/
