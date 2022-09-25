84a93182ddd9e5176802b3531e90ad045759ffd3
charizardmigo <fomechepriestly7@gmail.com>
2022-09-25 21:33:00 +0100

    Solution to Exercise 4

---
+++ c-modern-approach/chapter8/exercises/exercise4.c
@@ -0,0 +1,15 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 8, Exercise 4, Page 177
+
+PRIESTLEY FOMECHE
+27TH AUGUST 2022
+
+Q:  (C99) Repeat Exercise 3, ut this timeuse a designated initializer. Make the initializer as short as possible.
+
+
+A:
+    bool weekend[7] = {true, [6] = true};
+
+*/
