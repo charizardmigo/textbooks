c235434e3d73e2cec74dde7a31e9885fcfe95726
charizardmigo <fomechepriestly7@gmail.com>
2022-09-25 21:32:14 +0100

    Solution to Exercise 3

---
+++ c-modern-approach/chapter8/exercises/exercise3.c
@@ -0,0 +1,16 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 8, Exercise 3, Page 177
+
+PRIESTLEY FOMECHE
+27TH AUGUST 2022
+
+Q:  Write a declaration of an array named weekend that contains seven bool values. Include an initializer that makes the
+    first and last values true; all other values should be false.
+
+
+A:
+    int weekend[7] = {1, 0, 0, 0, 0, 0, 1};
+
+*/
