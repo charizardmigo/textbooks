3157aa94497a35dfaef901f24fd06b66b3603f0b
charizardmigo <fomechepriestly7@gmail.com>
2022-09-26 16:11:11 +0100

    Solution to Exercise 7

---
+++ c-modern-approach/chapter8/exercises/exercise7.c
@@ -0,0 +1,24 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 8, Exercise 7, Page 178
+
+PRIESTLEY FOMECHE
+27TH AUGUST 2022
+
+Q:  Using the shortcuts described in Section 8.2, shrink the initializer for the segments array (Exercise 6) as much as you can.
+
+
+A:  const int segments[10][7] = {{1, 1, 1, 1, 1, 1},
+                                 {0, 1, 1},
+                                 {1, 1 ,0, 1, 1, 0, 1},
+                                 {1, 1, 1, 1, 0, 0, 1},
+                                 {0, 1, 1, 0, 0, 1, 1},
+                                 {1, 0, 1, 1, 0, 1, 1},
+                                 {1, 0, 1, 1, 1, 1, 1},
+                                 {1, 1, 1},
+                                 {1, 1, 1, 1, 1, 1, 1},
+                                 {1, 1, 1, 1, 0, 1, 1}};
+
+
+*/
