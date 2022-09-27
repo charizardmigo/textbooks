c4c25d3b676e59fb2bf4e64fd24dab262e7c22db
charizardmigo <fomechepriestly7@gmail.com>
2022-09-27 22:35:57 +0100

    Solution to Exercise 8

---
+++ c-modern-approach/chapter8/exercises/exercise8.c
@@ -0,0 +1,17 @@
+/*
+
+C Programming: A Modern Approach (Second Edition)
+Chapter 8, Exercise 8, Page 178
+
+PRIESTLEY FOMECHE
+27TH AUGUST 2022
+
+Q:  Write a declaration for a two-dimensional array named temperature_readings that stores one month of hourly temperature readings.
+    (For simplicity, assume that a month has 30 days). The rows of the array should represent days of the month; the columns should
+    represent hours of the day.
+
+
+A:  double temperature_readings[30][24] = {0.0};
+
+
+*/
